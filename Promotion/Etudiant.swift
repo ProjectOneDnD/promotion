1d325d661f36c0218006daedf83d8bb9590a6c89
qr0wproject <fclaeys.project@gmail.com>
2016-06-20 13:40:06 +0200

    add class Etudiant

---
+++ Promotion/Etudiant.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class Etudiant: Personne {
+    var bourse: Bool
+    var anneeEtude: Int
+
+    init(nom: String, prenom: String, age: Int, bourse: Bool, anneeEtude: Int) {
+        self.bourse = bourse
+        self.anneeEtude = anneeEtude
+        super.init(nom: nom, prenom: prenom, age: age)
+    }
+
+    override func affiche() {
+        super.affiche()
+        if bourse {
+            print("L'étudiant a une bourse")
+        } else {
+            print("L'étudiant n'a pas de bourse")
+        }
+        print("Année d'étude : + \(self.anneeEtude)")
+    }
+
+}
